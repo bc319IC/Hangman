f9c1576b336b0462a762abcc039db29d564d8cfb
root <root@DESKTOP-OC0T85H>
2024-01-22 11:49:00 +0000

    updated repository according to feedback

--- README.md
+++ README.md
@@ -10,10 +10,11 @@ This is an implementation of the Hangman game, where the computer thinks of a wo
 - License Information
 
 ## Installation
-None, just clone the repository.
+None, just clone the repository using the command `git clone
+https://github.com/bc319IC/Hangman.git` in the terminal.
 
 ## Usage
-Run the appropraite files in the following section regarding their descriptions.
+Run the appropraite files in the following section regarding their descriptions by typing `python3` followed by the filename in the terminal.
 
 ## File Structure
 - milestone_2.py
@@ -30,7 +31,10 @@ Implementation of the Hangman class which takes in a word list to start the game
 
 - milestone_5.py
 
-Completed version of the Hangman game against the computer with the default number of lives set to 5.
+Completed version of the Hangman game against the computer with the default number of lives set to 5 with a word list already provided which can be altered before starting the program according to the user's needs. 
+
+ - game logic
+
+The hangman class consists of initialising the class attributes followed by two class functions of which one checks whether the guessed letter is a valid/repeated input and the other checks whether the guessed letter is in the word and changes the number of lives accordingly. Upon guessing the word or running out of lives, the game is ended.
 
 ## License Information
-Template provided by AiCore
--- hangman/milestone_5.py
+++ hangman/milestone_5.py
@@ -3,6 +3,7 @@ import random
 class Hangman():
     
     def __init__(self, word_list, num_lives=5):
+        '''Initialiases the class attributes'''
         self.word_list = word_list
         self.word = random.choice(self.word_list)
         self.word_guessed = ['_']*len(self.word)
@@ -11,22 +12,23 @@ class Hangman():
         self.list_of_guesses = []
 
     def check_guess(self, guess):
-        guess = guess.lower()
+        '''Checks whether the guessed letter is in the word and changes the number of lives accordingly'''
         if guess in self.word:
             print(f"Good guess! {guess} is in the word")
             for idx, letter in enumerate(self.word):
                 if guess == letter:
                     self.word_guessed[idx] = guess
                     self.num_letters -=1
-            #print(self.word_guessed)
         else:
             self.num_lives -=1
             print(f"Sorry, {guess} is not in the word.\nYou have {self.num_lives} lives left.\n")
         print(self.word_guessed)
 
     def ask_for_input(self):
+        '''Checks whether the guessed letter is a valid/repeated input'''
         while True:
             guess = input("guess a letter\n")
+            guess = guess.lower()
             if len(guess) != 1 or guess.isalpha() == False:
                 print("Invalid letter. Please, enter a single alphabetical character.")
             elif guess in self.list_of_guesses:
@@ -34,22 +36,19 @@ class Hangman():
             else:
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
-                break
+                if self.num_letters == 0 or self.num_lives == 0:
+                    break
 
 def play_game(word_list):
     num_lives = 5
     game = Hangman(word_list, num_lives)
     print(game.word_guessed)
-    while True:
-        #print(game.num_lives,game.num_letters)
-        if game.num_lives == 0:
-            print("You lost!")
-            break
-        elif game.num_letters > 0:
-            game.ask_for_input()
-        else:
-            print("Congratulations. You won the game!")
-            break
+    if game.num_letters > 0:
+        game.ask_for_input()
+    if game.num_lives == 0:
+        print("You lost!")
+    if game.num_letters == 0:
+        print("Congratulations. You won the game!")
 
 if __name__ == '__main__':
     word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
